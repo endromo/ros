e97f2e602530d7a82876c9f7aa5b62b3aefe624a
endromo <endro.mono@outlook.com>
2024-04-15 06:28:37 +0700

    update

--- app/routes/contact.js
+++ app/routes/contact.js
@@ -5,4 +5,14 @@
 import Route from '@ember/routing/route';
 
 export default class ContactRoute extends Route {
+    model() {
+        let result = [{
+            id: 1,
+            name: 'Endro',
+            email: 'endro.mono9@gmail.com'
+        }];
+
+        return result;
+
+    }
 }
