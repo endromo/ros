f04ade7fe25decc9afdae926293b57db414664b5
endromo <endro.mono@outlook.com>
2024-04-16 09:00:18 +0700

    update

--- app/router.js
+++ app/router.js
@@ -15,4 +15,5 @@ Router.map(function () {
   this.route('home', {path:'/'});
   this.route('login');
   this.route('signup');
+  this.route('customer');
 });
--- app/routes/login.js
+++
@@ -1,8 +0,0 @@
-//  Created By: Endro 
-//  Email: endro.mono9@gmail.com 
-//  Created At: 2024-04-12 
-
-import Route from '@ember/routing/route';
-
-export default class LoginRoute extends Route {
-}
