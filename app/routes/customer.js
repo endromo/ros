e0af5dd6121d3d6fca2e42aac8933f234c37235c
endromo <endro.mono@outlook.com>
2024-04-16 09:06:00 +0700

    update

--- app/routes/customer.js
+++ app/routes/customer.js
@@ -1,4 +1,14 @@
 import Route from '@ember/routing/route';
 
 export default class CustomerRoute extends Route {
+    model() {
+        let result = [{
+            id: 1,
+            username: 'Endro',
+            pwd: '123'
+        }];
+
+        return result;
+
+    }
 }
