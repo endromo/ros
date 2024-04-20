96c56d18038ff334a8b854070eb7860d54f7d579
endromo <endro.mono@outlook.com>
2024-04-20 16:08:01 +0700

    update

--- app/controllers/customer.js
+++ app/controllers/customer.js
@@ -1,8 +1,13 @@
 import Controller from "@ember/controller";
+import Route from '@ember/routing/route';
 import { action } from "@ember/object";
 import { tracked } from "@glimmer/tracking";
+// import service from '@ember/service';
+import { inject as service } from '@ember/service';
+
 
 export default class CustomerController extends Controller {
+  @service router;
   @tracked username = "user1";
   @tracked pwd = "";
 
@@ -10,6 +15,8 @@ export default class CustomerController extends Controller {
   customerOn() {
     console.log(this.username);
     alert(this.username.toString());
+    this.router.transitionTo("customer.show", this.username);
+    // this.router.transitionTo("customer", this.username);
   }
 
   @action
