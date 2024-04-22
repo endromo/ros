4de483c5a2a4fe2d52862c9b09209391078f88dd
endromo <endro.mono@outlook.com>
2024-04-22 12:22:55 +0700

    update

--- frontend/app/routes/customer.ts
+++ frontend/app/routes/customer.ts
@@ -1,4 +1,6 @@
+import { isArray } from "@ember/array";
 import Route from "@ember/routing/route";
+import { isEmpty, isNone } from "@ember/utils";
 import config from "ros/config/environment";
 
 export default class CustomerRoute extends Route {
@@ -6,15 +8,45 @@ export default class CustomerRoute extends Route {
     let response = await fetch(url, config.fetchOpt);
 
     let parsed = await response.json();
+
     console.log("reqGet parsed");
     console.log(parsed);
 
-    return parsed;
+    if (parsed.length == 0) {
+      console.log("reqGet parsed.length");
+      console.log(parsed.length);
+
+      let customers = [
+        {
+          id: "1",
+          username: "user1",
+          pwd: "1231",
+        },
+        {
+          id: "2",
+          username: "user2",
+          pwd: "1232",
+        },
+      ];
+
+      console.log("reqGet customers");
+      console.log(customers);
+
+      return customers;
+    } else {
+      return parsed;
+    }
   }
 
   async model() {
-    let parsed = this.reqGet(config.loopBackURL + "/api/customers");
+    console.log("url model");
+    console.log(config.loopBackURL + "/api/customers");
 
-    return parsed;
+    let promise = this.reqGet(config.loopBackURL + "/api/customers");
+
+    console.log("reqGet model promise");
+    console.log(promise);
+
+    return promise;
   }
 }
